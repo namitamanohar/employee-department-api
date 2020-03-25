1f0ac7b385daa2f41464f649cc3d7e5b8488c8ef
namitamanohar <manohar.namita@gmail.com>
2020-03-25 14:49:55 -0500

    first commit making an api from SQL database

---
+++ MyEmployeeDepartmentAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using MyEmployeeDepartmentAPI.Models;
+using Microsoft.AspNetCore.Http;
+
+
+namespace MyEmployeeDepartmentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class employeesController : ControllerBase
+    {
+
+        private readonly IConfiguration _config;
+
+        public employeesController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    SELECT e.Id, e.FirstName, e.LastName, e.DepartmentId, d.Id, d.DeptName
+                    FROM Employee e 
+                    LEFT JOIN Department d 
+                    ON d.Id = e.DepartmentId";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Employee> employees = new List<Employee>();
+
+                    while (reader.Read())
+                    {
+                        Employee employee = new Employee
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            DepartmentId= reader.GetInt32(reader.GetOrdinal("DepartmentId")),
+                            Department = new Department
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
+                                DeptName = reader.GetString(reader.GetOrdinal("DeptName"))
+                            }
+
+                        };
+
+                        employees.Add(employee);
+                    }
+                    reader.Close();
+
+                    return Ok(employees);
+                }
+            }
+        }
+
+        [HttpGet("{id}", Name = "GetEmployee")]
+        public async Task<IActionResult> Get([FromRoute] int Id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    SELECT e.Id, e.FirstName, e.LastName, e.DepartmentId, d.Id AS DepartmentId, d.DeptName
+                    FROM Employee e 
+                    LEFT JOIN Department d 
+                    ON d.Id = e.DepartmentId
+                    WHERE e.Id = @id";
+
+                    cmd.Parameters.Add(new SqlParameter("@id", Id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Employee employee = null;
+
+                    if (reader.Read())
+                    {
+                         employee = new Employee
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            DepartmentId = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
+                             Department = new Department
+                             {
+                                 Id = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
+                                 DeptName = reader.GetString(reader.GetOrdinal("DeptName"))
+                             }
+
+                         };
+                    }
+                    reader.Close();
+
+                    return Ok(employee);
+                }
+            }
+        }
+    }
+}
---
+++ MyEmployeeDepartmentAPI/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyEmployeeDepartmentAPI.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DeptName { get; set; }
+        public List<Employee> Employees { get; set; }
+
+    }
+}
